1f904c177370e8a356f6e9f2c8bee08da63402e8
vives <vsvivevives@gmail.com>
2019-08-07 23:54:16 +0530

    Added

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'tutorial-2',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-217-217-247.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/Homely.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:vives/tutorial-pt-2',
+      path: '/home/ubuntu/tutorial-2',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
